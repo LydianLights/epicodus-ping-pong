40b0f1d650b397c86c6ae453818d604756347671
LydianLights <rane.fields@gmail.com>
2017-09-22 10:01:43 -0700

    Replace multiples of 3 and 5 with 'ping' and 'pong' respectively

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,15 @@ function pingPong(countLimit) {
   }
   var output = [];
   for (var i = 1; i <= countLimit; i++) {
-    output.push(i);
+    if (i % 3 === 0) {
+      output.push("ping");
+    }
+    else if (i % 5 === 0) {
+      output.push("pong");
+    }
+    else {
+      output.push(i);
+    }
   }
   return output;
 }
