501345351c1750755da04c42a2b8b43db64b6109
LydianLights <rane.fields@gmail.com>
2017-09-22 09:41:30 -0700

    Add basic error checking to handle non-number user inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
+// Generates Ping Pong game list up to given limit
+// Returns null if non-positive-integer is passed in
+function pingPong(countLimit) {
+  if (/[^\d]/.test(countLimit) || countLimit < 1) {
+    return null;
+  }
+
+  return countLimit;
+}
+
+
+
+
+
+
+
 $(document).ready(function() {
   $("#game-settings form").submit(function(event) {
     event.preventDefault();
     var countLimit = $("#game-settings input[name=count-limit]").val();
-    var output = countLimit;
+    var output = pingPong(countLimit);
 
-    $("#game-output li").text(output);
-    $("#game-output").show();
+    if (output !== null) {
+      $("#game-output li").text(output);
+      $("#game-output").show();
+    }
+    else {
+      $("#game-output").hide();
+    }
   });
 });
